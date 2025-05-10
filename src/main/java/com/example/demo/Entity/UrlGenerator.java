79b9e5430d056098a6fc7397939490c2beb00dcb
Murad Mammadli <mammamur@fit.cvut.cz>
2025-05-11 03:25:21 +0400

    Implemented functionality where users can select the date and currency code to retrieve data.

--- src/main/java/com/example/demo/Entity/UrlGenerator.java
+++ src/main/java/com/example/demo/Entity/UrlGenerator.java
@@ -5,11 +5,22 @@ import java.time.format.DateTimeFormatter;
 
 //Current date appended to URL, to get current date's xml
 public class UrlGenerator {
-    public static String generateTodayUrl(){
+
+    //generate string from LocalDate with today's date
+    public static String generateTodayUrl() {
         LocalDate today = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String formattedDate = today.format(formatter);
 
         return "https://cbar.az/currencies/" + formattedDate + ".xml";
     }
+
+    //generate string from LocalDate with selected date
+    public static String generateUrlByDate(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String formattedDate = date.format(formatter);
+
+        return "https://cbar.az/currencies/" + formattedDate + ".xml";
+    }
+
 }
--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,29 +1,52 @@
 package com.example.demo;
 
 import com.example.demo.Entity.*;
+
 import java.io.InputStream;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 /**
-This class reads "az.xml" file, parses it into a {@link ValCurs} object using {@link Parser} class
- and prints the currency exchange info to the console.
+ * This class reads XML file, parses it into a {@link ValCurs} object using {@link Parser} class
+ * and prints the currency exchange info to the console.
  */
 public class Main {
     public static void main(String[] args) {
         try {
-            //endpoint of Central Bank of Azerbaijan with appended current date
-            String URL = UrlGenerator.generateTodayUrl();
+            System.out.print("PLease Enter date in format (dd.MM.yyyy): ");
+            Scanner scanner = new Scanner(System.in);
+            String dateInput = scanner.nextLine();
+
+            // Parse date
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            LocalDate date = LocalDate.parse(dateInput, formatter);
+
+            //endpoint of Central Bank of Azerbaijan with appended selected date
+            String URL = UrlGenerator.generateUrlByDate(date);
             InputStream stream = new URL(URL).openStream();
             ValCurs valCurs = Parser.parse(stream);
 
-            for (ValType valType : valCurs.getValTypes()) {
-                System.out.println(valType.getType());
-                for (Valute valute : valType.getValutes()) {
-                    System.out.println(valute.getCode() + ": Nominal " + valute.getNominal() + ": " + valute.getName() + ": " + valute.getValue());
-                }
-                System.out.println();
-            }
+            //selecting currency code
+            System.out.println("Please Select Currency Code: ");
+            Scanner scanner1 = new Scanner(System.in);
+            String filter = scanner1.nextLine().trim().toLowerCase();
 
+            //lists all currency codes... not sure if this is necessary
+//            for (ValType valType: valCurs.getValTypes()){
+//                for(Valute valute : valType.getValutes()){
+//                    System.out.println(valute.getCode());
+//                }
+//            }
+
+            for (ValType valType : valCurs.getValTypes()) {
+                for (Valute valute : valType.getValutes()) {
+                    if (valute.getCode().equalsIgnoreCase(filter)) {
+                        System.out.println(valute.getCode() + ": Nominal " + valute.getNominal() + ": " + valute.getName() + ": " + valute.getValue());
+                    }
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
